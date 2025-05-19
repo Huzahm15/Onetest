af397226196ef1401d648b99e61e095297b9e31a
Huzahm15 <huzaifaahmed@rocketmail.com>
2025-05-19 13:47:10 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', //  <-- TRY THIS FIRST
+  // or base: '/', // <--- THEN TRY THIS if './' doesn't work
 })
